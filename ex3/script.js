51b5cc7ee2cf00a0e3092d7dc046c4510781deaa
dianna1234 <01234dianna@gmail.com>
2022-02-28 22:56:02 +0400

    changed

--- ex3/script.js
+++ ex3/script.js
@@ -7,52 +7,95 @@ const publishYearList = document.querySelector(".publishYear");
 const tbody = document.querySelector("tbody");
 const pagination = document.querySelector(".pagination");
 
-let url = 'http://openlibrary.org/search.json?q=';
+let spaces  = input.value.replaceAll(" ", "+");
+let url = `http://openlibrary.org/search.json?q=${spaces}`;
+
 
 button.addEventListener("click", (e)=>{
     e.preventDefault();
-    if(input.value.includes(" ")){
-        let repValue = input.value.replace(" ", "+");
-        let newUrl = url + repValue;
-        newUrl.toString()
 
-        url = newUrl;
-    
-    }
-    
-    fetch(`${url+input.value}`).then((res)=>{
-        return res.json();
-        }).then((r)=>{
-            
-        console.log(r);
-        tbody.innerHTML = "";
-        r.docs.forEach((el)=>{
-            let listValues = [el.title, el.author_name, el.first_publish_year, el.subject];
-            let tr = document.createElement("tr");
-            listValues.forEach((val)=>{
-                let td = document.createElement("td");
-                let attr = document.createAttribute('class');
-                attr.value = 'data';
-                td.setAttributeNode(attr)
-                td.innerText = val;
-                tr.appendChild(td);
-            })
-    
-        
-            tbody.appendChild(tr)
-    
-        })
-        let pages = Math.ceil(r.numFound/100)
-        pagination.innerHTML = '';
-        for (let i = 1; i <= pages; i++) {
-            let pageNumber = document.createElement("button");
-            pageNumber.classList = 'pageNum';
-            pageNumber.innerText = i;
-            pagination.append(pageNumber);
-          }
-        searchRes.innerText =`Search resaults are equal ${r.numFound}`;
-        searchRes.style.color = '#fff';
-    
-        })
+    bookFounder()   
 })
 
+function bookFounder(){
+    fetch(`${url+input.value}`).then((res)=>{
+
+        return res.json();
+
+    }).then((r)=>{
+            
+            console.log(r);
+            tbody.innerHTML = "";
+            r.docs.forEach((el)=>{
+                let listValues = [el.title, el.author_name, el.first_publish_year, el.subject];
+                let tr = document.createElement("tr");
+                listValues.forEach((val)=>{
+                    let td = document.createElement("td");
+                            let attr = document.createAttribute('class');
+                            attr.value = 'data';
+                            td.setAttributeNode(attr);
+                            td.innerText = val;
+                            tr.appendChild(td);
+ 
+                })
+        
+                tbody.appendChild(tr)
+        
+            })
+
+            searchRes.innerText =`Search resaults are equal ${r.numFound}`;
+            searchRes.style.color = '#fff';
+
+            let pages = Math.ceil(r.numFound/100)
+            pagination.innerHTML = '';
+            paginate(pages);
+       
+        })
+
+}
+
+function paginate(pages){
+    for (let i = 1; i <= pages; i++) {
+            
+        let pageNumber = document.createElement("button");
+        pageNumber.classList = 'pageNum';
+        pageNumber.innerText = i;
+        pageNumber.addEventListener('click',(e)=>{
+          
+            if(i === 1){
+
+                bookFounder()
+                        
+            }else  {
+               
+                fetch(`http://openlibrary.org/search.json?q=&page=${e.target.value}`).then((res)=>{
+
+                    return res.json();
+
+                }).then((res)=>{
+                            console.log(res);
+                            tbody.innerHTML = "";
+                            res.docs.forEach((el)=>{
+                                let listValues = [el.title, el.author_name, el.first_publish_year, el.subject];
+                                let tr = document.createElement("tr");
+                                listValues.forEach((val)=>{
+                                    let td = document.createElement("td");
+                                    let attr = document.createAttribute('class');
+                                    attr.value = 'data';
+                                    td.setAttributeNode(attr)
+                                    td.innerText = val;
+                                    tr.appendChild(td);
+                                    tbody.appendChild(tr)
+                                })
+                            })
+    
+                        })
+                
+            }
+             
+
+        })
+     
+        pagination.append(pageNumber);
+      }
+}
